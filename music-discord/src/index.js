ac67a16f2500722237cd60f59fc66a9d68a7d6cd
Artoria01 <79462135+Artoria01@users.noreply.github.com>
2021-03-09 18:37:44 +0100

    Add files via upload

---
+++ music-discord/src/index.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const { readdirSync } = require('fs');
+const { join } = require('path');
+const MusicClient = require('./struct/Client');
+const { Collection } = require('discord.js');
+const client = new MusicClient({ token: process.env.DISCORD_TOKEN, prefix: process.env.DISCORD_PREFIX });
+
+const commandFiles = readdirSync(join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(join(__dirname, 'commands', `${file}`));
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => console.log('READY!'));
+client.on('message', message => {
+	if (!message.content.startsWith(client.config.prefix) || message.author.bot) return;
+	const args = message.content.slice(client.config.prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	if (!command) return;
+	if (command.guildOnly && message.channel.type !== 'text') return message.reply('Je ne peux pas exécutez cette commande dans les DM!');
+	if (command.args && !args.length) {
+		let reply = `Vous n'avez fourni aucun argument, ${message.author}!`;
+		if (command.usage) reply += `\nL'utilisation appropriée serait: \`${client.config.prefix}${command.name} ${command.usage}\``;
+		return message.channel.send(reply);
+	}
+	if (!client.cooldowns.has(command.name)) {
+		client.cooldowns.set(command.name, new Collection());
+	}
+	const now = Date.now();
+	const timestamps = client.cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`S'il vous plaît, attendez ${timeLeft.toFixed(1)} plus de seconde (s) avant de réutiliser le \`${command.name}\` commande.`);
+		}
+	}
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('il y a eu une erreur en essayant d\'exécuter cette commande!');
+	}
+});
+
+client.login(client.config.token);
